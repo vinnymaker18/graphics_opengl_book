1c45d54cee447a482f31b3425cceb216db806f42
Vinay Emani <vinnymaker18@gmail.com>
2022-02-08 00:00:11 +0530

    Initial working version

--- programs/test.cpp
+++ programs/test.cpp
@@ -16,13 +16,13 @@ GLuint createShaderProgram() {
   const char *vshaderSource = 
     "#version 410 \n"
     "void main(void) \n"
-    "{gl_Position = vec4(0.0, 0.0, 0.0, 1.0);}";
+    "{gl_Position = vec4(0.0, 0.0, 1.0, 1.0);}";
 
   const char *fshaderSource = 
     "#version 410 \n"
     "out vec4 color; \n"
     "void main(void) \n"
-    "{color = vec4(0.0, 0.0, 1.0, 1.0);";
+    "{if (gl_FragCoord.x < 600) color = vec4(1, 1, 1, 1); else color = vec4(0, 0, 1, 1);}";
 
   GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
   GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -49,8 +49,10 @@ void init(GLFWwindow* window)
 
 void display(GLFWwindow *window, double currentTime)
 {
+  glClearColor(1.0, 0.0, 0.0, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT);
   glUseProgram(renderingProgram);
-  glPointSize(10.0);
+  glPointSize(30.0f);
   glDrawArrays(GL_POINTS, 0, 1);
 }
 
