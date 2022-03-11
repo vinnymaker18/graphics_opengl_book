f01c92f1cf6a1c67a60c94932716f3186245bc4d
Vinay Emani <vinnymaker18@gmail.com>
2022-03-11 15:25:24 +0530

    utils and errors modules moved to common directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 
 # e.g.,
 # include "GLFW/glfw3.h", "glm/glm.hpp" "GL/glew.h" "SOIL2/SOIL2.h"
-g++ -I glfw-3.3.6/include -I glm -I glew/include -I soil2/src -I programs\
+g++ -I glfw-3.3.6/include -I glm -I glew/include -I soil2/src -I common\
   -L glfw-3.3.6/build/src -L soil2/lib/macosx -L glew/lib\
   -l glfw -l GLEW -l soil2-debug\
   -framework OpenGL\
   -o $2\
-  programs/errors.cpp programs/utils.cpp\
+  common/errors.cpp common/utils.cpp\
   $1
 
 # Need to set DYLD_LIBRARY_PATH before we can run the binaries.
